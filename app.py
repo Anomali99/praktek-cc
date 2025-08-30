cd56e8ff63363e093a9386b1d3131dfc83f53202
Anomali99 <fatiqnur@gmail.com>
2025-08-30 17:15:39 +0700

    add home page

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Halo! Aplikasi Flask ini dideploy secara otomatis dari GitHub Actions ke VPS!'
-
+    return render_template("base.html")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
