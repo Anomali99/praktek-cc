f7237c32dfcb18b595d00a042cd5b6a79437ad9b
Anomali99 <fatiqnur@gmail.com>
2025-08-30 17:58:57 +0700

    add about page

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("base.html")
 
 @app.route('/')
-def hello_world():
+def about():
     return render_template("about.html")
 
 
