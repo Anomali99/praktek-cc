f54d08ecc3b5f4d3a7e5c549871de8c60cdfc521
Anomali99 <fatiqnur@gmail.com>
2025-08-30 17:27:25 +0700

    add about page

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("base.html")
+
+@app.route('/')
+def hello_world():
+    return render_template("about.html")
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
